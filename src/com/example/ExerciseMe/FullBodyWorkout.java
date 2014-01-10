cbf2f8807521fcb8bba2e5a2abc438a545294949
Phil Godzin <pgg2105@columbia.edu>
2014-01-10 18:33:16 -0500

    Working demo completed - notifaction and on completed workout text visibility added.

--- src/com/example/ExerciseMe/FullBodyWorkout.java
+++ src/com/example/ExerciseMe/FullBodyWorkout.java
@@ -1,13 +1,15 @@
 package com.example.ExerciseMe;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 
 public class FullBodyWorkout extends Activity {
@@ -25,22 +27,9 @@ public class FullBodyWorkout extends Activity {
                 intent.setData(Uri.parse("http://www.amazon.com/Under-Armour-Short-Sleeve-T-Shirt/dp/B0073GO64U/ref=sr_1_13?ie=UTF8&qid=1389349267&sr=8-13&keywords=workout+clothes"));
                 startActivity(intent);
             }
-        });
-        boolean cb1Checked, cb2Checked, cb3Checked, cb4Checked;
-        TextView doneTV = (TextView) findViewById(R.id.done);
-        final CheckBox cb1 = (CheckBox) findViewById(R.id.checkBox);
-        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-               // if (cb1.isChecked()) cb1Checked = true;   TODO: Figure out!
-            }
-        });
-        final CheckBox cb2 = (CheckBox) findViewById(R.id.checkBox2);
-        final CheckBox cb3 = (CheckBox) findViewById(R.id.checkBox3);
-        final CheckBox cb4 = (CheckBox) findViewById(R.id.checkBox4);
 
-        if (cb1.isChecked() && cb2.isChecked() && cb3.isChecked() && cb4.isChecked())
-            doneTV.setVisibility(View.VISIBLE);
+
+        });
 
         Button b2 = (Button) findViewById(R.id.shortsButton);
         b2.setOnClickListener(new View.OnClickListener() {
@@ -68,5 +57,25 @@ public class FullBodyWorkout extends Activity {
 
     }
 
+    public void onCheckboxClicked(View view) {
 
+        TextView doneTV = (TextView) findViewById(R.id.done);
+        final CheckBox cb1 = (CheckBox) findViewById(R.id.checkBox);
+        final CheckBox cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        final CheckBox cb3 = (CheckBox) findViewById(R.id.checkBox3);
+        final CheckBox cb4 = (CheckBox) findViewById(R.id.checkBox4);
+
+        if (cb1.isChecked() && cb2.isChecked() && cb3.isChecked() && cb4.isChecked()){
+            doneTV.setVisibility(View.VISIBLE);
+            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification.Builder mBuilder =
+                    new Notification.Builder(this)
+                            .setSmallIcon(android.R.drawable.stat_sys_warning)
+                            .setContentTitle("Sore after workout?")
+                            .setContentText("Try supplements now!");
+            nm.notify(0, mBuilder.build());
+
+        }
+
+    }
 }
